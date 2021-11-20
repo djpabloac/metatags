e658717a8fb96013efa2af4d4ea152bce4bc2efe
Pablo Arteaga <dj.pablo.ac@gmail.com>
2021-11-20 13:50:07 -0500

    Update layout size lg

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,22 +18,22 @@ export default function Layout() {
     return (
         <Fragment>
             <Navbar />
-            <Box component='main'>
+            <Box component='main' sx={{minWidth: '900px'}}>
                 <Grid container spacing={0}>
-                    <Grid item xs={3} sx={{
+                    <Grid item xs={3} lg={2} xl={2} sx={{
                         ...classes.borderGrid,
                         paddingLeft: '24px',
                         paddingTop: '24px',
                     }}>
                         <Siderbar />
                     </Grid>
-                    <Grid item xs={4} sx={{
+                    <Grid item xs={4} lg={5} xl={5} sx={{
                         ...classes.contentGrid,
                         ...classes.borderGrid,
                     }}>
                         <MetaTagDetail />
                     </Grid>
-                    <Grid item xs={5} sx={{
+                    <Grid item xs={5} lg={5} xl={5} sx={{
                         ...classes.contentGrid,
                         ...classes.borderGrid,
                         paddingRight: '24px',
