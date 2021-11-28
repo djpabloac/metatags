a75af1e2ff0cd1863be8fdefe6387f7a130bd8e1
Pablo Arteaga <dj.pablo.ac@gmail.com>
2021-11-27 19:14:34 -0500

    [update] package.json for heroku

--- src/components/Siderbar.tsx
+++ src/components/Siderbar.tsx
@@ -23,6 +23,7 @@ export default function Siderbar() {
             if(sn.checked)
                 addSelected(sn);
         }))
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     const handlerSocial = (sn: SocialNetworkType) => {
